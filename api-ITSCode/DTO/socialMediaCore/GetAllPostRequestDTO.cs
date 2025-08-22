8195255a87bb29367a4075996cfd3b220a860e9a
HernandezCarlosIsaias <120617047+HernandezCarlosIsaias@users.noreply.github.com.>
2025-08-21 22:06:10 -0300

    Actualizo controlador post

--- api-ITSCode/DTO/socialMediaCore/GetAllPostRequestDTO.cs
+++ api-ITSCode/DTO/socialMediaCore/GetAllPostRequestDTO.cs
@@ -1,8 +1,4 @@
 public class GetAllPostResquestDTO
 {
-    public long pageSize { get; set; }
-    public long pageNumber { get; set; }
-    public string query { get; set; }
-    public string orderBy { get; set; }
-    public string direction { get; set;}
+
 }
