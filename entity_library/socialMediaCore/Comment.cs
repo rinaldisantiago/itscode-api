1e1a91c875102b6b7b9ea01815dfb25c8ea768f1
Santiago Tomas Rinaldi <santiago.rinaldi@itscordoba.edu.ar>
2025-09-20 15:47:30 -0300

    Cambio en nombre del archivo

---
+++ entity_library/socialMediaCore/Comment.cs
@@ -0,0 +1,18 @@
+using entity_library;
+using Microsoft.VisualBasic;
+
+public class Comment
+{
+    private int id;
+    private User? user;
+    private Post? post;
+    private string? content;
+
+
+    public int Id { get => this.id; set => this.id = value; }
+    public string? Content { get => this.content; set => this.content = value; }
+    public Post? Post { get { return this.post; } set { this.post = value; } }
+    public User? User { get { return this.user; } set { this.user = value; } }
+
+    
+}
