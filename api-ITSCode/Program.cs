82e56801fba2b5df5fbc6f8f84ce3088d916506d
Luis Romano <luisromano@gmail.com>
2025-12-11 18:15:05 -0300

    Ref: Agregado EnsureCreated() en Program.cs

--- api-ITSCode/Program.cs
+++ api-ITSCode/Program.cs
@@ -31,6 +31,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
